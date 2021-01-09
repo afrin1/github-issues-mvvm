a0d93fefe118429d58f332195c399ca6a39755d1
afrinsulthana <afrinsulthana.ali@thoughtworks.com>
2021-01-09 22:55:43 +0530

    Add Progress bar and Databinding with ViewModel

--- app/src/main/java/com/project/githubissues/ui/issueList/IssueListFragment.kt
+++ app/src/main/java/com/project/githubissues/ui/issueList/IssueListFragment.kt
@@ -1,14 +1,15 @@
 package com.project.githubissues.ui.issueList
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.RecyclerView
+import androidx.lifecycle.ViewModelProvider
 import com.project.githubissues.R
+import com.project.githubissues.databinding.FragmentIssueListBinding
 
 class IssueListFragment : Fragment() {
 
@@ -17,23 +18,25 @@ class IssueListFragment : Fragment() {
     }
 
     private lateinit var viewModel: IssueListViewModel
-    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(R.layout.fragment_issue_list, container, false)
+        val binding: FragmentIssueListBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_issue_list, container, false
+        )
         val adapter = IssueAdapter()
-        recyclerView= view.findViewById(R.id.issues_list)
-        recyclerView.adapter = adapter
-        viewModel = IssueListViewModel()
-        viewModel.issues.observe(viewLifecycleOwner, Observer{
+        binding.recyclerView.adapter = adapter
+
+        val issueListViewModel = ViewModelProvider(this).get(IssueListViewModel::class.java)
+        binding.issueListViewModel = issueListViewModel
+        issueListViewModel.issues.observe(viewLifecycleOwner, Observer {
             it?.let {
                 adapter.data = it
             }
         })
-        return view
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/com/project/githubissues/ui/issueList/IssueListViewModel.kt
+++ app/src/main/java/com/project/githubissues/ui/issueList/IssueListViewModel.kt
@@ -5,14 +5,16 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class IssueListViewModel : ViewModel() {
-    private val issue = Issue(1, "Title", "Description", "updated At", "username", "vatar url")
-    private val issue2 = Issue(2, "Title 2", "Description 2", "updated At 2", "username 2", "vatar url 2")
     private val _issues = getIssues()
+    val isProgressVisible: Boolean = true
     val issues: LiveData<List<Issue>>
         get() = _issues
 
     private fun getIssues(): MutableLiveData<List<Issue>> {
-        var data: MutableLiveData<List<Issue>> =  MutableLiveData()
+        var data: MutableLiveData<List<Issue>> = MutableLiveData()
+        val issue = Issue(1, "Title", "Description", "updated At", "username", "vatar url")
+        val issue2 =
+            Issue(2, "Title 2", "Description 2", "updated At 2", "username 2", "vatar url 2")
         data.value = listOf(issue, issue2)
         return data
     }
