a0116b8d9731cd90a659ad1b1031e148fc813158
afrinsulthana <afrinsulthana.ali@thoughtworks.com>
2021-01-10 14:27:33 +0530

    Add Livedata to load issues with Tests

--- app/src/main/java/com/project/githubissues/model/issuelist/Issue.kt
+++ app/src/main/java/com/project/githubissues/model/issuelist/Issue.kt
@@ -1,10 +1,16 @@
 package com.project.githubissues.ui.issueList
 
+import com.google.gson.annotations.SerializedName
+
 data class Issue(
     val id: Long = 0L,
     val title: String,
+    @SerializedName("body")
     val description: String,
+    @SerializedName("updated_at")
     val updatedAt: String,
+    @SerializedName("login")
     val userName: String,
+    @SerializedName("avatar_url")
     val avatar: String
 )
--- app/src/main/java/com/project/githubissues/ui/issueList/Util.kt
+++
@@ -1,3 +0,0 @@
-package com.project.githubissues.ui.issueList
-
-//class IssueItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
